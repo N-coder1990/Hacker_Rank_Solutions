4c9214df64a001be4545ae8ad471caebb691d0bc
N-coder1990 <69002516+N-coder1990@users.noreply.github.com>
2020-07-31 16:55:58 +0300

    Add files via upload

---
+++ hr_day11.py
@@ -0,0 +1,38 @@
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+
+    n = int(input())
+    result = bin(n)
+    br=result[2:]
+    #print(br)
+    k=1
+    m=[]
+    for i in range(len(br)-1):
+
+        if int(br[i])==1:
+            
+            if int(br[i+1])==1:
+                k=k+1
+            
+            else:
+                
+                k=1
+        m.append(k)
+        
+seq=max(m)
+#print(m)
+        
+print(seq)
+    
+
+    
+    
+
+
+
+
